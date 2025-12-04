edb2e6e50af39a7576eac29d9884b902a1fa1367
mochensky <162050937+mochensky@users.noreply.github.com>
2025-12-04 16:34:29 +0300

    Add files via upload

---
+++ mc1_21_6-1_21_8/src/main/java/com/mochensky/deadlyconnection/DeadlyConnection.java
@@ -0,0 +1,31 @@
+package com.mochensky.deadlyconnection;
+
+import net.fabricmc.api.ModInitializer;
+import net.fabricmc.fabric.api.entity.event.v1.ServerLivingEntityEvents;
+import net.minecraft.server.network.ServerPlayerEntity;
+import net.minecraft.server.world.ServerWorld;
+
+public class DeadlyConnection implements ModInitializer {
+    private static boolean isProcessingDeath = false;
+
+    @Override
+    public void onInitialize() {
+        ServerLivingEntityEvents.AFTER_DEATH.register((entity, damageSource) -> {
+            if (entity instanceof ServerPlayerEntity && !isProcessingDeath) {
+                isProcessingDeath = true;
+                try {
+                    ServerPlayerEntity deadPlayer = (ServerPlayerEntity) entity;
+                    ServerWorld world = deadPlayer.getWorld();
+
+                    for (ServerPlayerEntity player : world.getServer().getPlayerManager().getPlayerList()) {
+                        if (player != deadPlayer && !player.isDead()) {
+                            player.kill(player.getWorld());
+                        }
+                    }
+                } finally {
+                    isProcessingDeath = false;
+                }
+            }
+        });
+    }
+}
